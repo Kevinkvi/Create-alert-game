638cae725262c77de27a28078c56eff0972a0195
aleks <104278634+Kevinkvi@users.noreply.github.com>
2023-01-02 23:37:57 +0600

    formativ javascript

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
 alert("Добро пожаловать в игру!");
-let name = prompt('Как тебя зовут ?');
+let name = prompt("Как тебя зовут ?");
 function ask(question, yes, no) {
-    if (confirm(question)) yes()
-    else no();
-  }
-  function showOk(){
-frens= prompt('Сколько у тебя друзей?')
-  };
-  function showCancel(){
-alert("Окей тогда увидемся");
-  }
-  ask(`${name} Я спрошу у тебя пару вопросов ты готов ?`,showOk,showCancel);
-  alert('да ты крут!');
-  let a=3 
-  b=frens
-  alert(`Если у тебя будет 3 кг креветок то по ровну на каждого друга будет:${a/b}`);
-  alert("У этого числа тип данных : number");
-  alert(`при делении 3 на ${b} остаток будет ${a%b}`);
-  alert(`3 в степени ${b}будет ${a**b}`);
+  if (confirm(question)) yes();
+  else no();
+}
+function showOk() {
+  frens = prompt("Сколько у тебя друзей?");
+}
+function showCancel() {
+  alert("Окей тогда увидемся");
+}
+ask(`${name} Я спрошу у тебя пару вопросов ты готов ?`, showOk, showCancel);
+alert("да ты крут!");
+let a = 3;
+b = frens;
+alert(
+  `Если у тебя будет 3 кг креветок то по ровну на каждого друга будет:${a / b}`
+);
+alert("У этого числа тип данных : number");
+alert(`при делении 3 на ${b} остаток будет ${a % b}`);
+alert(`3 в степени ${b}будет ${a ** b}`);
