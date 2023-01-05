1fc92b75a3d8fca74e35766a12896462c2c1d920
aleks <104278634+Kevinkvi@users.noreply.github.com>
2023-01-05 22:57:14 +0600

    function

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
+(function () {
+  "use strict";
+})();
+
 alert("Добро пожаловать в игру!");
-let name = prompt("Как тебя зовут ?");
+let UserName;
+do {
+  UserName = prompt("ваше имя");
+} while (UserName <= null);
+
 function ask(question, yes, no) {
   if (confirm(question)) yes();
   else no();
 }
 function showOk() {
-  frens = prompt("Сколько у тебя друзей?");
+  let number = prompt("Сколько у тебя друзей?", "");
+  alert("да ты крут!");
+  let a = 3,
+    b = number;
+  alert(
+    `Если у тебя будет 3 кг креветок то по ровну на каждого друга будет: ${
+      a / b
+    }`
+  );
+  alert(`при деление 3 на ${b} остаток будет : ${a % b}`);
+  alert(`3 в степени ${b} будет:${a ** b}`);
 }
 function showCancel() {
   alert("Окей тогда увидемся");
 }
-ask(`${name} Я спрошу у тебя пару вопросов ты готов ?`, showOk, showCancel);
-alert("да ты крут!");
-let a = 3;
-b = frens;
-alert(
-  `Если у тебя будет 3 кг креветок то по ровну на каждого друга будет:${a / b}`
-);
-alert("У этого числа тип данных : number");
-alert(`при делении 3 на ${b} остаток будет ${a % b}`);
-alert(`3 в степени ${b}будет ${a ** b}`);
+ask(`${UserName} Я спрошу у тебя пару вопросов ты готов ?`, showOk, showCancel);
