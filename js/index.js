e232987b2ab726ff2408418f37a8971f7461046e
aleks <104278634+Kevinkvi@users.noreply.github.com>
2023-01-10 00:25:42 +0600

    while

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ alert("Добро пожаловать в игру!");
 let UserName;
 do {
   UserName = prompt("ваше имя");
-} while (UserName <= null);
+} while (UserName === "");
 
 function ask(question, yes, no) {
   if (confirm(question)) yes();
@@ -13,10 +13,10 @@ function showOk() {
   let number;
   do {
     number = prompt("Сколько у тебя друзей?");
-  } while (number <= 100000 && 0);
+  } while (number === NaN);
   alert("да ты крут!");
-  let a = 3,
-    b = number;
+  let a = 3;
+  let b = number;
   alert(
     `Если у тебя будет 3 кг креветок то по ровну на каждого друга будет: ${
       a / b
